0e0dc0168e34a5dff7398cd1284ae83b145643a4
haianh1233 <anhndhse151389@fpt.edu.vn>
2022-06-02 17:11:05 +0700

    update nlog + swagger

--- Bussiness/Program.cs
+++ Bussiness/Program.cs
@@ -1,7 +1,9 @@
 using DataAccess.Repository;
 using DataAccess.Repository.MySqlRepository;
+using NLog;
 using tutoring_online_be.Services;
 using tutoring_online_be.Services.V1;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,23 +18,32 @@ builder.Services.AddSwaggerGen();
 //Subject
 builder.Services.AddSingleton<ISubjectService, SubjectServiceV1>();
 builder.Services.AddSingleton<ISubjectDao, SubjectDao>();
+
 //Lesson
 builder.Services.AddSingleton<ILessonService, LessonServiceV1>();
 builder.Services.AddSingleton<ILessonDao, LessonDao>();
+
 //Syllabus
 builder.Services.AddSingleton<ISyllabusService, SyllabusServiceV1>();
 builder.Services.AddSingleton<ISyllabusDao, SyllabusDao>();
+
 //Payment
 builder.Services.AddSingleton<IPaymentService, PaymentServiceV1>();
 builder.Services.AddSingleton<IPaymentDao, PaymentDao>();
 var app = builder.Build();
 
+//Setup logger
+NLog.Common.InternalLogger.LogLevel = NLog.LogLevel.Trace;
+NLog.Common.InternalLogger.LogToConsole = true;
+Logger logger = LogManager.GetLogger("Logger");
+logger.Info("Program started");
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+//Swagger config
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
